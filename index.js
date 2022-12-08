d06a3f6c0621ba0dbdce377e6799afbf92932ff3
+ <+>
2022-12-08 10:17:33 +0300

    complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// Removing the node element here 
+document.querySelector("main#main").remove();
+
+// Creating a new element and assigning it to newHeader 
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "STEVEN is the champion";
+newHeader.className = "victory";
